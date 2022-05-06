2bc36b603e0d0fba97d1973cfa9af8245e07d1c1
ANGELES DE LUZ <segara968@gmail.com>
2022-05-05 19:51:23 -0500

    My first commit

--- app/src/main/java/com/example/ova/MainActivity4.java
+++ app/src/main/java/com/example/ova/MainActivity4.java
@@ -15,7 +15,7 @@ public class MainActivity4 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main4);
         btnpvs = findViewById(R.id.pvs);
         btnpvs.setOnClickListener(new View.OnClickListener() {
             @Override
